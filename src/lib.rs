9649bc34bbbd62c11d7dc0f01d40ddaabd2b5547
Flavio Castelli <fcastelli@suse.com>
2021-01-29 10:49:47 +0100

    Prototype the new msg format
    The validate msg is now a json object with this structure:
    
      {
        "settings": {} <- the settings of the policy
        "request": {} <- the actual k8s request to eval
      }
    
    The policy has been changed to have a `valid_namespace` setting that
    defines which requests should be accepted.
    Before we had this value hard coded.

--- src/lib.rs
+++ src/lib.rs
@@ -15,17 +15,45 @@ pub(crate) struct ValidationResponse {
     pub code: Option<u16>,
 }
 
-fn validate(req: &[u8]) -> CallResult {
-    let req_obj: serde_json::Value = serde_json::from_slice(req)?;
+fn validate(validation_req: &[u8]) -> CallResult {
+    let validation_req_obj: serde_json::Value = serde_json::from_slice(validation_req)?;
+    let settings = validation_req_obj.get("settings");
 
-    let resp: ValidationResponse = match req_obj.get("namespace") {
+    let resp: ValidationResponse;
+
+    if settings.is_none() {
+        resp = ValidationResponse {
+            accepted: false,
+            message: Some(String::from("No settings provided to the policy")),
+            code: Some(500),
+        };
+        return Ok(serde_json::to_vec(&resp)?);
+    }
+    let settings = settings.unwrap();
+
+    let valid_namespace = settings.get("valid_namespace");
+    if valid_namespace.is_none() {
+        resp = ValidationResponse {
+            accepted: false,
+            message: Some(String::from(
+                "Warning: 'valid_namespace' parameter missing from policy settings",
+            )),
+            code: None,
+        };
+        return Ok(serde_json::to_vec(&resp)?);
+    }
+
+    let valid_namespace = valid_namespace.unwrap().as_str().unwrap();
+    let req_obj = validation_req_obj.get("request").unwrap();
+
+    resp = match req_obj.get("namespace") {
         None => ValidationResponse {
             accepted: false,
             message: Some(String::from("namespace missing from admission request")),
             code: Some(400),
         },
         Some(namespace) => ValidationResponse {
-            accepted: (namespace.as_str().unwrap() == "valid"),
+            accepted: (namespace.as_str().unwrap() == valid_namespace),
             message: None,
             code: None,
         },
